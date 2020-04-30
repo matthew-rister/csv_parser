8ec8977f19286d31c0f42c2ce3fbe24e04dda9e7
Matthew Rister <matthewrister@outlook.com>
2020-04-29 21:55:09 -0700

    feat: add row header parser

--- csv_parser/src/csv.hpp
+++ csv_parser/src/csv.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -9,13 +10,31 @@ namespace csv {
 	template <typename T> class Csv {
 
 	public:
-		explicit Csv(const std::string& data) {
-			for (const auto& line : read_lines(data)) {
-				entries_.push_back(read_line(line));
+		explicit Csv(const std::string& data, const bool has_row_header = false) {
+
+			const auto lines = read_lines(data);
+			auto begin = lines.begin();
+
+			if (has_row_header && begin != lines.end()) {
+				row_header_ = read_line<std::string>(lines[0]);
+				++begin;
 			}
+
+			std::transform(begin, lines.end(), std::back_inserter(entries_), [](const auto& line) {
+				return read_line(line);
+			});
 		}
 
 		friend std::ostream& operator<<(std::ostream& os, const Csv& csv) {
+			if (!csv.row_header_.empty()) {
+				for (std::size_t i = 0; i < csv.row_header_.size(); ++i) {
+					os << csv.row_header_[i];
+					if (i < csv.row_header_.size() - 1) {
+						os << ", ";
+					}
+				}
+				os << '\n';
+			}
 			for (std::size_t i = 0; i < csv.entries_.size(); ++i) {
 				for (std::size_t j = 0; j < csv.entries_[i].size(); ++j) {
 					os << csv.entries_[i][j];
@@ -40,23 +59,24 @@ namespace csv {
 			return lines;
 		}
 
-		static std::vector<T> read_line(const std::string& line) {
-			std::vector<T> tokens;
+		template <typename U = T> static std::vector<U> read_line(const std::string& line) {
+			std::vector<U> tokens;
 			std::istringstream iss{line};
 
 			for (std::string token; std::getline(iss, token, ',');) {
-				T t;
-				std::istringstream{token} >> t;
-				if constexpr (std::is_move_assignable<T>::value) {
-					tokens.push_back(std::move(t));
+				U u;
+				std::istringstream{token} >> u;
+				if constexpr (std::is_move_assignable<U>::value) {
+					tokens.push_back(std::move(u));
 				} else {
-					tokens.push_back(t);
+					tokens.push_back(u);
 				}
 			}
 
 			return tokens;
 		}
 
+		std::vector<std::string> row_header_;
 		std::vector<std::vector<T>> entries_;
 	};
 }
