a700bd9b9e350d8dc13f70f31adc558ccc6e7db9
Matthew Rister <matthewrister@outlook.com>
2020-04-29 20:09:01 -0700

    refactor: rename csv parser to csv and add namespace

---
+++ csv_parser/src/csv.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace csv {
+
+	class Csv {
+	};
+}
--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -1,6 +1,8 @@
 #include "catch.hpp"
 
-#include "csv_parser.hpp"
+#include "csv.hpp"
+
+using namespace csv;
 
 TEST_CASE("2 + 2 = 4") {
 	REQUIRE(2 + 2 == 5);
