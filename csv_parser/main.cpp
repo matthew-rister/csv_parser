352eca4f2a0308422825b6427b41d37cceb36c44
Matthew Rister <matthewrister@outlook.com>
2020-05-01 21:45:59 -0700

    feat: create dynamic template parsing prototype.

--- csv_parser/main.cpp
+++ csv_parser/main.cpp
@@ -1,23 +1,39 @@
-//#define CATCH_CONFIG_MAIN
-//
-//#include "catch.hpp"
-
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
-#include "csv.hpp"
+template <typename...>
+struct TypeList {};
 
-using namespace csv;
+std::tuple<> parse(TypeList<>, const std::vector<std::string>&, const std::size_t) {
+	return {};
+}
+
+template <typename T>
+T parse(const std::vector<std::string>& v, const std::size_t i) {
+	T t;
+	std::istringstream{v[i]} >> t;
+	return t;
+}
+
+template<typename T, typename ... Rest>
+std::tuple<T, Rest...> parse(TypeList<T, Rest...>, const std::vector<std::string>& v, const std::size_t i) {
+	const auto t1 = std::make_tuple(parse<T>(v, i));
+	const auto t2 = parse(TypeList<Rest...>{}, v, i + 1);
+	return std::tuple_cat(t1, t2);
+}
+
+template<typename... Types>
+std::tuple<Types...> parse(const std::vector<std::string>& v) {
+	return parse(TypeList<Types...>{}, v, 0);
+}
 
 int main() {
 
-	try {
-		const std::string data{"1, 2, 3\n4, 5, 6\n7, 8, 9"};
-		const Csv<uint8_t> csv{data};
-		std::cout << csv << std::endl;
-	} catch(...) {
-		return EXIT_FAILURE;
-	}
+	const auto tokens = std::vector<std::string>{ "a", "1", "3.14" }
+	const auto [c, i, d] = parse<char, int, double>(tokens);
+	std::cout << c << ' ' << i << ' ' << d;
 
 	return 0;
 }
