133bc31cd396e239f65a017d171a6600da8fc7e6
Matthew Rister <matthewrister@outlook.com>
2020-05-01 21:49:48 -0700

    style: code cleanup

--- csv_parser/main.cpp
+++ csv_parser/main.cpp
@@ -10,22 +10,20 @@ std::tuple<> parse(TypeList<>, const std::vector<std::string>&, const std::size_
 	return {};
 }
 
-template <typename T>
-T parse(const std::vector<std::string>& v, const std::size_t i) {
+template <typename T> std::tuple<T> parse(const std::vector<std::string>& v, const std::size_t i) {
 	T t;
 	std::istringstream{v[i]} >> t;
-	return t;
+	return std::make_tuple(t);
 }
 
 template<typename T, typename ... Rest>
 std::tuple<T, Rest...> parse(TypeList<T, Rest...>, const std::vector<std::string>& v, const std::size_t i) {
-	const auto t1 = std::make_tuple(parse<T>(v, i));
-	const auto t2 = parse(TypeList<Rest...>{}, v, i + 1);
-	return std::tuple_cat(t1, t2);
+	return std::tuple_cat(
+		parse<T>(v, i),
+		parse(TypeList<Rest...>{}, v, i + 1));
 }
 
-template<typename... Types>
-std::tuple<Types...> parse(const std::vector<std::string>& v) {
+template<typename... Types> std::tuple<Types...> parse(const std::vector<std::string>& v) {
 	return parse(TypeList<Types...>{}, v, 0);
 }
 
