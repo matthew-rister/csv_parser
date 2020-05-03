e236c2722f98e8827710b0851d5fb0a23f2f62d4
Matthew Rister <matthewrister@outlook.com>
2020-05-03 00:21:02 -0700

    test: add additional parsing tests

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -2,17 +2,50 @@
 
 #include "csv.hpp"
 
+#include <iostream>
+
 using namespace csv;
 
-TEST_CASE("Output streamming", "[csv]") {
+TEST_CASE("CSV parsing", "[csv]") {
 
-	const std::string data{ "a, 3.14, 1, 1.12\nb, 3.15, 2, 1.13\nc, 3.16, 3, 1.14" };
-	Csv<char, double, int, double> csv{ data };
+	const std::string data{"a, 3.14, 42, true\nb, 2.72, 0, false"};
+	const Csv<char, double, int, bool> csv{data};
 
-	SECTION("Printing should be equal to the original") {
-		REQUIRE(csv.ToString() == data);
+	SECTION("The first row is correct") {
+
+		SECTION("The first element is correct") {
+			REQUIRE(csv.Get<char>(0, 0) == 'a');
+		}
+
+		SECTION("The second element is correct") {
+			REQUIRE(csv.Get<double>(0, 1) == 3.14);
+		}
+
+		SECTION("The third element is correct") {
+			REQUIRE(csv.Get<int>(0, 2) == 42);
+		}
+
+		SECTION("The fourth element is correct") {
+			REQUIRE(csv.Get<bool>(0, 3) == true);
+		}
 	}
 
+	SECTION("The second row is correct") {
 
-	REQUIRE(csv.Get<double>(1, 3) == 1.13);
+		SECTION("The first element is correct") {
+			REQUIRE(csv.Get<char>(1, 0) == 'b');
+		}
+
+		SECTION("The second element is correct") {
+			REQUIRE(csv.Get<double>(1, 1) == 2.72);
+		}
+
+		SECTION("The third element is correct") {
+			REQUIRE(csv.Get<int>(1, 2) == 0);
+		}
+
+		SECTION("The fourth element is correct") {
+			REQUIRE(csv.Get<bool>(1, 3) == false);
+		}
+	}
 }
