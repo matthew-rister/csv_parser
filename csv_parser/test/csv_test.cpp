cd3675481e5795c800b1c7c2fb159c43e1bf1939
Matthew Rister <matthewrister@outlook.com>
2020-05-02 21:48:33 -0700

    fix: build break

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -9,5 +9,5 @@ TEST_CASE("Output streamming", "[csv]") {
 	const std::string data{"a, 3.14, 1, 1.12\nb, 3.15, 2, 1.13\n, 3.16, 3, 1.14"};
 	Csv<char, double, int, double> csv{data};
 
-	REQUIRE(csv.get<double>(1, 3) == 1.13);
+	REQUIRE(csv.Get<double>(1, 3) == 1.13);
 }
