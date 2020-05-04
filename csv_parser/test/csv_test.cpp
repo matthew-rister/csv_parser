855a49ea5d8a3f2c7cb0cb813f8141adb015b004
Matthew Rister <matthewrister@outlook.com>
2020-05-03 21:28:56 -0700

    test: use int64_t for numeric limits test

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -65,20 +65,26 @@ TEST_CASE("CSV Initialization", "[csv]") {
 	}
 
 	SECTION("Initializing a CSV with numeric limits") {
-		std::stringstream data;
-		data << std::numeric_limits<int32_t>::max() << ", " << std::numeric_limits<double>::max() << std::endl
-			<< std::numeric_limits<int32_t>::min() << ", " << std::numeric_limits<double>::min();
 
-		const Csv<int32_t, double> csv{data};
+		constexpr auto int64_t_max = std::numeric_limits<int64_t>::max();
+		constexpr auto int64_t_min = std::numeric_limits<int64_t>::min();
+		constexpr auto double_max = std::numeric_limits<double>::max();
+		constexpr auto double_min = std::numeric_limits<double>::max();
+
+		std::stringstream data;
+		data << int64_t_max << ", " << double_max << std::endl
+			<< int64_t_min << ", " << double_min;
+
+		const Csv<int64_t, double> csv{data};
 
 		SECTION("Parsing integer numeric limits is correct") {
-			REQUIRE(csv.Get<int32_t>(0, 0) == std::numeric_limits<int32_t>::max());
-			REQUIRE(csv.Get<int32_t>(1, 0) == std::numeric_limits<int32_t>::min());
+			REQUIRE(csv.Get<int64_t>(0, 0) == int64_t_max);
+			REQUIRE(csv.Get<int64_t>(1, 0) == int64_t_min);
 		}
 
 		SECTION("Parsing double numeric limits is correct") {
-			REQUIRE(csv.Get<double>(0, 1) == Approx{std::numeric_limits<double>::max()});
-			REQUIRE(csv.Get<double>(1, 1) == Approx{std::numeric_limits<double>::min()});
+			REQUIRE(csv.Get<double>(0, 1) == Approx{double_max});
+			REQUIRE(csv.Get<double>(1, 1) == Approx{double_min});
 		}
 	}
 }
