297a51de67960b104201a6b17f2e2398b685f5e1
Matthew Rister <matthewrister@outlook.com>
2020-05-03 22:39:43 -0700

    refactor: restructure test organization

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -4,14 +4,14 @@
 
 using namespace csv;
 
-TEST_CASE("CSV Initialization", "[csv]") {
+TEST_CASE("CSV parsing with homogeneous data", "[csv]") {
 
-	SECTION("Initializing a CSV with an empty input stream does not throw an exception") {
+	SECTION("Parsing an empty input stream does not throw an exception") {
 		std::stringstream data;
-		REQUIRE_NOTHROW(Csv<>{data});
+		REQUIRE_NOTHROW(Csv<int32_t>{data});
 	}
 
-	SECTION("Initializing a CSV with homogeneous data") {
+	SECTION("Parsing an input sream with multiple rows and columns is correct") {
 		std::stringstream data;
 		data << "0, 1, 2" << std::endl
 			<< "3, 4, 5" << std::endl
@@ -19,16 +19,22 @@ TEST_CASE("CSV Initialization", "[csv]") {
 
 		const Csv<int32_t> csv{data};
 
-		SECTION("Accessing CSV elements returns the correct element") {
-			for (auto i = 0; i < 3; ++i) {
-				for (auto j = 0; j < 3; ++j) {
-					REQUIRE(csv.Get(i, j) == 3 * i + j);
-				}
+		for (auto i = 0; i < 3; ++i) {
+			for (auto j = 0; j < 3; ++j) {
+				REQUIRE(csv.Get(i, j) == 3 * i + j);
 			}
 		}
 	}
+}
 
-	SECTION("Initializing a CSV with heterogeneous data") {
+TEST_CASE("CSV parsing with heterogeneous data") {
+
+	SECTION("Parsing an empty input stream does not throw an exception") {
+		std::stringstream data;
+		REQUIRE_NOTHROW(Csv<>{data});
+	}
+
+	SECTION("Parsing an input sream with multiple rows and columns") {
 		std::stringstream data;
 		data << "a, 3.141, 42, true" << std::endl
 			<< "b, 2.718, 0, false" << std::endl
@@ -36,35 +42,32 @@ TEST_CASE("CSV Initialization", "[csv]") {
 
 		const Csv<char, double, int32_t, bool> csv{data};
 
-		SECTION("Accessing CSV elements returns the correct element") {
+		SECTION("Character parsing is correct") {
+			REQUIRE(csv.Get<char>(0, 0) == 'a');
+			REQUIRE(csv.Get<char>(1, 0) == 'b');
+			REQUIRE(csv.Get<char>(2, 0) == 'c');
+		}
 
-			SECTION("Accessing character elements is correct") {
-				REQUIRE(csv.Get<char>(0, 0) == 'a');
-				REQUIRE(csv.Get<char>(1, 0) == 'b');
-				REQUIRE(csv.Get<char>(2, 0) == 'c');
-			}
+		SECTION("Double parsing is correct") {
+			REQUIRE(csv.Get<double>(0, 1) == Approx(3.141));
+			REQUIRE(csv.Get<double>(1, 1) == Approx(2.718));
+			REQUIRE(csv.Get<double>(2, 1) == Approx(1.618));
+		}
 
-			SECTION("Accessing double floating point elements is correct") {
-				REQUIRE(csv.Get<double>(0, 1) == Approx(3.141));
-				REQUIRE(csv.Get<double>(1, 1) == Approx(2.718));
-				REQUIRE(csv.Get<double>(2, 1) == Approx(1.618));
-			}
+		SECTION("Integer parsing is correct") {
+			REQUIRE(csv.Get<int32_t>(0, 2) == 42);
+			REQUIRE(csv.Get<int32_t>(1, 2) == 0);
+			REQUIRE(csv.Get<int32_t>(2, 2) == 7);
+		}
 
-			SECTION("Accessing integer elements is correct") {
-				REQUIRE(csv.Get<int32_t>(0, 2) == 42);
-				REQUIRE(csv.Get<int32_t>(1, 2) == 0);
-				REQUIRE(csv.Get<int32_t>(2, 2) == 7);
-			}
-
-			SECTION("Accessing boolean elements is correct") {
-				REQUIRE(csv.Get<bool>(0, 3) == true);
-				REQUIRE(csv.Get<bool>(1, 3) == false);
-				REQUIRE(csv.Get<bool>(2, 3) == true);
-			}
+		SECTION("Boolean parsing is correct") {
+			REQUIRE(csv.Get<bool>(0, 3) == true);
+			REQUIRE(csv.Get<bool>(1, 3) == false);
+			REQUIRE(csv.Get<bool>(2, 3) == true);
 		}
 	}
 
-	SECTION("Initializing a CSV with numeric limits") {
+	SECTION("Parsing an input stream with numeric limits") {
 		constexpr auto int64_t_max = std::numeric_limits<int64_t>::max();
 		constexpr auto int64_t_min = std::numeric_limits<int64_t>::min();
 		constexpr auto double_max = std::numeric_limits<double>::max();
@@ -82,8 +85,8 @@ TEST_CASE("CSV Initialization", "[csv]") {
 		}
 
 		SECTION("Parsing double numeric limits is correct") {
-			REQUIRE(csv.Get<double>(0, 1) == Approx{double_max});
-			REQUIRE(csv.Get<double>(1, 1) == Approx{double_min});
+			REQUIRE(csv.Get<double>(0, 1) == Approx{ double_max });
+			REQUIRE(csv.Get<double>(1, 1) == Approx{ double_min });
 		}
 	}
 }
