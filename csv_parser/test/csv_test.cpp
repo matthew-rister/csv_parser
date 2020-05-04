1ce38ae5532144d0ad8e1e064b6e0e486843a14e
Matthew Rister <matthewrister@outlook.com>
2020-05-03 20:27:24 -0700

    test: add homogeneous data test

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -5,7 +5,6 @@
 using namespace csv;
 
 namespace {
-
 	template <typename... ColumnTypes>
 	std::string ToString(const Csv<ColumnTypes...>& csv) {
 		std::ostringstream oss;
@@ -16,9 +15,25 @@ namespace {
 
 TEST_CASE("CSV Initialization", "[csv]") {
 
-	SECTION("Initializing a CSV with an empty input stream") {
+	SECTION("Initializing a CSV with an empty input stream does not throw an exception") {
 		std::stringstream data;
-		const Csv<> csv{data};
-		REQUIRE(ToString(csv) == std::string{});
+		REQUIRE_NOTHROW(Csv<>{data});
+	}
+
+	SECTION("Initializing a CSV with homogeneous data") {
+		std::stringstream data;
+		data << "0, 1, 2" << std::endl
+			<< "3, 4, 5" << std::endl
+			<< "6, 7, 8";
+
+		const Csv<int, int, int> csv{data};
+
+		SECTION("Accessing CSV elements returns the correct element") {
+			for (int i = 0; i < 3; ++i) {
+				for (int j = 0; j < 3; ++j) {
+					REQUIRE(csv.Get<int>(i, j) == 3 * i + j);
+				}
+			}
+		}
 	}
 }
