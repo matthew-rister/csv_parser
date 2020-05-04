c31a16dd525bb2dccde61d5397f2f9044fda9b44
Matthew Rister <matthewrister@outlook.com>
2020-05-03 20:41:29 -0700

    refactor: remove unused helper method

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -4,15 +4,6 @@
 
 using namespace csv;
 
-namespace {
-	template <typename... ColumnTypes>
-	std::string ToString(const Csv<ColumnTypes...>& csv) {
-		std::ostringstream oss;
-		oss << csv;
-		return oss.str();
-	}
-}
-
 TEST_CASE("CSV Initialization", "[csv]") {
 
 	SECTION("Initializing a CSV with an empty input stream does not throw an exception") {
