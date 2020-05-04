76e9bf49fe22d9c2a31fce85defca532f26f5592
Matthew Rister <matthewrister@outlook.com>
2020-05-03 20:38:21 -0700

    test: add heterogeneous data test

--- csv_parser/test/csv_test.cpp
+++ csv_parser/test/csv_test.cpp
@@ -36,4 +36,40 @@ TEST_CASE("CSV Initialization", "[csv]") {
 			}
 		}
 	}
+
+	SECTION("Initializing a CSV with heterogeneous data") {
+		std::stringstream data;
+		data << "a, 3.141, 42, true" << std::endl
+			<< "b, 2.718, 0, false" << std::endl
+			<< "c, 1.618, 7, true";
+
+		const Csv<char, double, int, bool> csv{data};
+
+		SECTION("Accessing CSV elements returns the correct element") {
+
+			SECTION("Accessing character elements is correct") {
+				REQUIRE(csv.Get<char>(0, 0) == 'a');
+				REQUIRE(csv.Get<char>(1, 0) == 'b');
+				REQUIRE(csv.Get<char>(2, 0) == 'c');
+			}
+
+			SECTION("Accessing double floating point elements is correct") {
+				REQUIRE(csv.Get<double>(0, 1) == 3.141);
+				REQUIRE(csv.Get<double>(1, 1) == 2.718);
+				REQUIRE(csv.Get<double>(2, 1) == 1.618);
+			}
+
+			SECTION("Accessing integer elements is correct") {
+				REQUIRE(csv.Get<int>(0, 2) == 42);
+				REQUIRE(csv.Get<int>(1, 2) == 0);
+				REQUIRE(csv.Get<int>(2, 2) == 7);
+			}
+
+			SECTION("Accessing boolean elements is correct") {
+				REQUIRE(csv.Get<bool>(0, 3) == true);
+				REQUIRE(csv.Get<bool>(1, 3) == false);
+				REQUIRE(csv.Get<bool>(2, 3) == true);
+			}
+		}
+	}
 }
